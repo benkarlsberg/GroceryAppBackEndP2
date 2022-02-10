2413bc1da6c5cdab3f6366b65b672f8c5742c78c
benkarlsberg <benelliot16@gmail.com>
2022-02-10 07:44:00 -0800

    Added Inital Spring Boot Project with Dependencies

---
+++ p2backend/src/main/java/com/revature/p2backend/P2backendApplication.java
@@ -0,0 +1,13 @@
+package com.revature.p2backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2backendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P2backendApplication.class, args);
+	}
+
+}
---
+++ p2backend/src/test/java/com/revature/p2backend/P2backendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.p2backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P2backendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
