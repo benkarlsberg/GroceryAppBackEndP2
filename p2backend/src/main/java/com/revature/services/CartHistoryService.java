00f3b18e8dc85e695e1d641668a12b1804ebc2db
Bobby <bodavis@csumb.edu>
2022-02-13 08:27:34 -0800

    Added getOrderHistory query

--- p2backend/src/main/java/com/revature/services/CartHistoryService.java
+++ p2backend/src/main/java/com/revature/services/CartHistoryService.java
@@ -2,7 +2,9 @@ package com.revature.services;
 import com.revature.models.CartHistory;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CartHistoryService {
     public List<CartHistory> getAllCartHistory();
+    public List <CartHistory> getOrderHistory(int userId);
 }
--- p2backend/src/main/java/com/revature/services/CartHistoryServiceImpl.java
+++ p2backend/src/main/java/com/revature/services/CartHistoryServiceImpl.java
@@ -3,9 +3,11 @@ package com.revature.services;
 import com.revature.models.CartHistory;
 import com.revature.repositories.CartHistoryRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CartHistoryServiceImpl implements CartHistoryService{
@@ -16,4 +18,11 @@ public class CartHistoryServiceImpl implements CartHistoryService{
     public List<CartHistory> getAllCartHistory() {
         return (List<CartHistory>) chr.findAll();
     }
+
+    @Override
+    public List <CartHistory> getOrderHistory(int userId) {
+        return chr.getOrderHistory(userId);
+    }
+
+
 }
