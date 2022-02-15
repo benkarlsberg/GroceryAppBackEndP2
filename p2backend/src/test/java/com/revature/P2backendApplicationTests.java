19a4e0aec9f3ee27c0515ab3d20c2472b6540d9f
Bobby <bodavis@csumb.edu>
2022-02-15 12:52:32 -0800

    Added P2backendApplication to driver package

--- p2backend/src/test/java/com/revature/P2backendApplicationTests.java
+++ p2backend/src/test/java/com/revature/P2backendApplicationTests.java
@@ -3,7 +3,7 @@ package com.revature;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = com.revature.driver.P2backendApplication.class)
 class P2backendApplicationTests {
 
 	@Test
